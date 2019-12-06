4e094215b2a4753fb8692fbef3a58cc96da55494
Alex Hodovas <o.hodovas@gmail.com>
2019-12-06 14:36:46 +0200

    Updates

---
+++ precache-manifest.75ebd2de1808f549d66b8d5544d27421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9472c301e8e540a7187de8a9c719c2f0",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "8223507a2fbb8f3ce00f",
+    "url": "/react_tabs/static/css/main.2d60da99.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "8223507a2fbb8f3ce00f",
+    "url": "/react_tabs/static/js/main.fe764432.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
